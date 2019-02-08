bcf8d102e3feab8dcc7aaa6affe8cf8f2d1f5aff
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-02-08 13:12:08 +0300

    Add index.php 2

--- index.php
+++ index.php
@@ -3,15 +3,20 @@ error_reporting(E_ALL);
 
 /*
  * Сбор информации о выручке
- * Генерация PDf
+ * Генерация PDF
  * Отправка на почту
  */
 
 // Секретный ключ вашего приложения берется из config.php
 $client_secret = '1547ba15d5dc931f291bbf8fdb5e8fa4';
 
-
+echo'<pre>';
 print_r($_GET['code']);
+echo'</pre>';
+
+
+echo'Настройки уведомления';
+
 
 // Отправляем уведомление
 //mail('storozhuk.nikita@gmail.com', 'Webhook Waiter', $text);
