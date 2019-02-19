8d55353bf3108dcb5bc5cccf336ac57689045a62
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-02-19 18:08:43 +0300

    Add fpdf 12

--- index.php
+++ index.php
@@ -53,17 +53,46 @@ $pdf->Output('reciept.pdf', 'F');
 
 $doc = $pdf->Output('reciept.pdf', 'S');
 
+require 'PHPMailer-master/src/Exception.php';
 require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
-$mail = new PHPMailer;
-$mail->setFrom('storozhuk.nikita@gmail.com', 'First Last');
-$mail->addAddress('storozhuk.nikita@gmail.com', 'John Doe');
-$mail->Subject = 'PHPMailer file sender';
-$mail->msgHTML("My message body");
-// Attach uploaded files
-//$mail->addAttachment($filename1);
-//$mail->addAttachment($filename2);
-$r = $mail->send();
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    /*$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'user@example.com';                 // SMTP username
+    $mail->Password = 'secret';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;   */                                 // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('storozhuk.nikita@gmail.com', 'Mailer');
+    $mail->addAddress('storozhuk.nikita@gmail.com', 'Joe User');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('storozhuk.nikita@gmail.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('reciept.pdf');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+
 
 //$mail->AddStringAttachment($doc, 'doc.pdf', 'base64', 'application/pdf');
 //$mail->Send();
