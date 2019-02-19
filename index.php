2e204784ae3f361526fc1c7171318de1a8855f00
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-02-19 17:26:49 +0300

    Add fpdf 3

--- index.php
+++ index.php
@@ -37,7 +37,57 @@ $pdf->SetFont('Arial', '', 12);
 
 $pdf->Cell(100, 16, "Hello, World!");
 
-$pdf->Output();
+$doc = $pdf->Output('S');
+
+$name        = "Название здесь идет";
+$email       = "storozhuk.nikita@gmail.com";
+$to          = "$name <$email>";
+$from        = "John Doe ";
+$subject     = "тема ";
+$mainMessage = "Привет,я сообщение с pdf файлом";
+$fileatt     = "doc.pdf"; // Расположение файла
+$fileatttype = "application/pdf";
+$fileattname = "newName.pdf"; //Имя, которое вы хотите использовать для отправки, или вы можете использовать то же имя
+$headers     = "From: $from";
+
+// Открываем и читаем файл в переменную.
+$file = fopen($fileatt, 'rb');
+$data = fread($file, filesize($fileatt));
+fclose($file);
+
+// Это прикрепляет файл
+$semi_rand     = md5(time());
+$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+$headers      .= "\nMIME-Version: 1.0\n" .
+  "Content-Type: multipart/mixed;\n" .
+  " boundary=\"{$mime_boundary}\"";
+  $message = "Это multi-part сообщение в формате MIME․\n\n" .
+  "-{$mime_boundary}\n" .
+  "Content-Type: text/plain; charset=\"iso-8859-1\n" .
+  "Content-Transfer-Encoding: 7bit\n\n" .
+  $mainMessage  . "\n\n";
+
+$data = chunk_split(base64_encode($data));
+$message .= "--{$mime_boundary}\n" .
+  "Content-Type: {$fileatttype};\n" .
+  " name=\"{$fileattname}\"\n" .
+  "Content-Disposition: attachment;\n" .
+  " filename=\"{$fileattname}\"\n" .
+  "Content-Transfer-Encoding: base64\n\n" .
+$data . "\n\n" .
+ "-{$mime_boundary}-\n";
+
+// Отправить письмо
+if(mail($to, $subject, $message, $headers))
+{
+    echo "Письмо отправлено.";
+} else {
+    echo "При отправке почты произошла ошибка.";
+}
+
+
+//$mail->AddStringAttachment($doc, 'doc.pdf', 'base64', 'application/pdf');
+//$mail->Send();
 
 /*
 echo'<pre>';
