25b7c78aed202c8a1b873733ee47d18c4c1c783b
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-02-21 17:35:49 +0300

    Debug poster-api 5

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ use poster\src\PosterApi;
 PosterApi::init([
     'application_id' => POSTER_CLIENT_ID, // Your application id (client_id)
     'application_secret' => POSTER_CLIENT_SECRET, // secret
-    'redirect_uri' => '/',
+    'redirect_uri' => 'https://bot.coffee-hub.ru/index.php',
 ]);
 
 
