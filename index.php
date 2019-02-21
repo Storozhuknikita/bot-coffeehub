48f8f147b6889a4b4196b79652babb6b5e1ff04a
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-02-21 17:41:22 +0300

    Debug poster-api 6

--- index.php
+++ index.php
@@ -23,13 +23,10 @@ use poster\src\PosterApi;
 PosterApi::init([
     'application_id' => POSTER_CLIENT_ID, // Your application id (client_id)
     'application_secret' => POSTER_CLIENT_SECRET, // secret
-    'redirect_uri' => 'https://bot.coffee-hub.ru/index.php',
 ]);
 
 
-$oAuthUrl = PosterApi::auth()->getOauthUrl();
-
-$result = (object)PosterApi::auth()->getOauthToken('coffeehubofficial', $_GET['code']);
+$result = (object)PosterApi::auth()->getOauthToken(PosterApi::getAccountName(), $_GET['code']);
 
 if (empty($result->access_token)) {
     echo "Poster auth error";
