8cf5e6d1982eb4284e60c0ff5af38d49c486000a
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-02-24 17:33:50 +0300

    Use Poster Api 17

--- config/config.php
+++ config/config.php
@@ -8,11 +8,26 @@ error_reporting(E_ALL);
 date_default_timezone_set('Europe/Moscow');
 
 
-
 function debug($var) {
     echo'<pre>';
     print_r($var);
     echo'</pre>';
 }
+
+
+// Poster Class для авторизации
+include_once '../class/Poster.class.php';
+
+// PDF Class
+require('../class/fpdf181/fpdf.php');
+
+// Template PDF
+include_once '../class/TemplatePDF.class.php';
+
+
+// MAIL Class
+require '../class/PHPMailer-master/src/Exception.php';
+require '../class/PHPMailer-master/src/PHPMailer.php';
+require '../class/PHPMailer-master/src/SMTP.php';
 
 ?>
